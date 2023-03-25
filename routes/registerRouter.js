0b40530e336dad4124eac57879612e16228d2b76
ChandiniShivanna <chandinigj@gmail.com>
2023-03-25 12:41:57 +0530

    login and registration done

--- routes/registerRouter.js
+++ routes/registerRouter.js
@@ -1,10 +1,10 @@
 const router=require('express').Router();
 const USer=require('../models/userSchema');
 const jwt = require('jsonwebtoken');
-const {body , validationResult} =require('express-vallidator');
+const {body , validationResult} = require('express-validator');
 const bcrypt = require('bcrypt');
 
-router.post("/register",body("email").isEmail(), async(req,res)=>{
+router.post("/register",body('email').isEmail(), async(req,res)=>{
     try{
         let {email,password,confirmPassword} = req.body;
         if(password!==confirmPassword){
@@ -14,40 +14,40 @@ router.post("/register",body("email").isEmail(), async(req,res)=>{
             })
         }
 
-        let response= await USer.find({email});
-        if(!response){
-            return res.status(403).json({
-                status:"Failed",
-                message:"User already Exists"
-            })
+        let isUser= await USer.findOne({email:email});
+        if(isUser){
+            return res.status(403).send("USer Already exists")
         }else{
-            bcrypt.hash(password,10,async(err,hash)=>{
-                if(err){
-                    return res.status(403).json({
-                        status:"Failed",
-                        message:"please signup again",
-                        err
-                    })
-                }
-            try{
-                let response= await USer.create({email:email,password:hash})
-                res.status(200).json({
-                    status:"Success",
-                    message:"User Created Successfully",
-                    response
+            const errors=validationResult(req);
+            if(!errors.isEmpty()){
+                return res.status(400).json({
+                    "error":errors.array()
                 })
-            }catch(e){
-                res.status(403).json({
-                    status:"Failed",
-                    message:e.message
+            }else{
+                bcrypt.hash(password,10,async function(err,hash){
+                    if(err){
+                        return res.status(400).json({
+                            "Error":err.message
+                        })
+                    }else{
+                        const user=new USer({
+                            email:email,
+                            password:hash,
+                        });
+                        user.save().then(()=>{
+                            return res.status(200).json({
+                                "user":user
+                            })
+                        })
+                    }
                 })
             }
-            })
         }
     }catch(e){
-        res.status(403).json({
-            status:"Failed",
-            message:e.message
-        })
+        return res.status(400).json({
+            "Message":e.message
+        });
     }
-});
+})
+  
+module.exports=router;
